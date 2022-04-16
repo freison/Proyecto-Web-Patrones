8f507ec39c5e3c704e60cdbb8f9e91d2fbb4f3a9
micha <micha@DESKTOP-0Q46MOE>
2022-04-16 17:40:06 -0600

    Revert "Merge branch 'master' of https://github.com/freison/Proyecto-Web-Patrones"
    This reverts commit 627ebc0d346c57747000a05a14df4439868886aa.

--- src/main/java/com/proyecto/controller/IndexController.java
+++ src/main/java/com/proyecto/controller/IndexController.java
@@ -1,11 +1,7 @@
 package com.proyecto.controller;
 
 
-import com.proyecto.domain.Articulo;
-import com.proyecto.service.ArticuloService;
-import com.proyecto.service.CarritoService;
 import com.proyecto.service.UsuarioService;
-import java.util.Hashtable;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,33 +16,10 @@ public class IndexController {
     @Autowired
     private UsuarioService usuarioService;
     
-    @Autowired
-    private CarritoService carritoService;
-    
-    @Autowired
-    private ArticuloService articuloService;
-    
     @GetMapping("/")
     public String inicio(Model model){
         var usuarios = usuarioService.getUsuarios();
         model.addAttribute("usuarios",usuarios);
         return "/index";
-    }
-    
-    @GetMapping("/tienda")
-    public String tienda(Model model){
-        
-        Hashtable<Articulo, Long> listaArticulos = carritoService.getArticulos();
-        int cantidadProductos = 0;
-        
-        for(var a : listaArticulos.entrySet()){
-            cantidadProductos += a.getValue();
-        }
-        
-        var articulos = articuloService.getArticulos();
-        model.addAttribute("articulos", articulos);
-        model.addAttribute("cantidadProductos", cantidadProductos);
-        
-        return "tienda";
     }
 }
